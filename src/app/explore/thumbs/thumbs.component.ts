90a3ab94cf65524410af061b992752a264fdd10c
Vadim Suleimanov <s1imus05@gmail.com>
2017-12-02 17:48:45 +0300

    Don't show filtered favourites

--- src/app/explore/thumbs/thumbs.component.ts
+++ src/app/explore/thumbs/thumbs.component.ts
@@ -1,28 +1,45 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input, OnInit, OnChanges, SimpleChanges} from '@angular/core';
 
 @Component({
   selector: 'app-thumbs',
   templateUrl: './thumbs.component.html',
   styleUrls: ['./thumbs.component.scss']
 })
-export class ThumbsComponent implements OnInit {
+export class ThumbsComponent implements OnInit, OnChanges {
 
   @Input() items: ExploreItem[];
   @Input() type: string;
   @Input() filter: string;
+  noFavourites: boolean;
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes['items']) {
+      this.checkForNoFavourites();
+    }
   }
 
   toggleFavourite(item: ExploreItem) {
     if (window.localStorage.getItem(item.id)) {
       window.localStorage.removeItem(item.id);
       item.favourite = false;
+      this.checkForNoFavourites();
     } else {
       window.localStorage.setItem(item.id, '1');
       item.favourite = true;
+      this.noFavourites = false;
     }
+  }
+
+  checkForNoFavourites() {
+    this.noFavourites = true;
+    this.items.forEach(f => {
+      this.noFavourites = this.noFavourites && !f.favourite;
+    });
   }
 }
